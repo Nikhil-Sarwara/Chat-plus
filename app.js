15eae6804f14dd5feb10587e869a085f00e483c6
Nikhil Sarwara <nikhil4818.be22@chitkara.edu.in>
2023-02-01 10:12:15 +0530

    emit total clients

--- app.js
+++ app.js
@@ -22,7 +22,21 @@ const server = app.listen(port, () => {
 // Socket IO Intialization
 const io = require("socket.io")(server);
 
+let connectedSockets = new Set();
+
 // When connected
-io.on("connection", (socket) => {
+io.on("connection", onConnected);
+
+function onConnected(socket) {
   console.log(socket.id);
-});
+  connectedSockets.add(socket.id);
+
+  io.emit("total", connectedSockets.size);
+
+  socket.on("disconnect", () => {
+    console.log("Socket Disconnected", socket.id);
+    connectedSockets.delete(socket.id);
+
+    io.emit("total", connectedSockets.size);
+  });
+}

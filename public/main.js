f2a46be584cfc7caa4edecd45866ecdd6661431e
Nikhil Sarwara <nikhil4818.be22@chitkara.edu.in>
2023-02-01 10:22:05 +0530

    emit in frontend

--- public/main.js
+++ public/main.js
@@ -1,5 +1,7 @@
 const socket = io();
 
+const numberOfClients = document.getElementById("total-client");
+
 socket.on("total", (data) => {
-  console.log(data);
+  numberOfClients.innerText = `Total Clients: ${data}`;
 });
